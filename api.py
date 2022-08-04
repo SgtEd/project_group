a32940837cc66dd487c8531ff7fb7fa987b4ab7e
sarahzulkifli <sssarahzlk@gmail.com>
2022-08-05 01:14:19 +0800

    error

--- api.py
+++ api.py
@@ -1,6 +1,25 @@
 import requests
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=8S58BDY13J9J9NK1'
-r = requests.get(url)
-data = r.json()
-print(data)
-exchange_rate=data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+import math
+from decimal import Decimal
+
+def getData():
+    print("Getting exchange rate..")
+
+    # declare url link
+    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=8S58BDY13J9J9NK1'
+
+    # get the data
+    r = requests.get(url)
+
+    # open data
+    data = r.json()
+
+
+    # print(data)
+
+    # access the Exchange Rate from the data
+    exchange_rate = Decimal(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+    roundedOffExchangeRate = round(exchange_rate, 8)
+
+    return roundedOffExchangeRate
+
